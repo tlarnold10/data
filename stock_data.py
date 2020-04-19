ab402485649a6ae8910cfb6b34ce96454dcc320f
tlarnold10@hotmail.com <tlarnold10@hotmail.com>
2020-04-19 09:00:55 -0500

    Adding some new stats for my linear regression application

--- stock_data.py
+++ stock_data.py
@@ -6,6 +6,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import argparse
+from sklearn.linear_model import LinearRegression
 
 parser = argparse.ArgumentParser(description='Create a chart for a given stock ticker')
 parser.add_argument('--ticker', help='The stock ticker you are wanting to look at.')
@@ -33,7 +34,7 @@ split_array = []
 for day in json_data['Time Series (Daily)']:
     date_array.append(day)
     # print('Open: ' + json_data['Time Series (Daily)'][day]['1. open'])
-    open_array.append(json_data['Time Series (Daily)'][day]['1. open'])
+    open_array.append(float(json_data['Time Series (Daily)'][day]['1. open']))
     # print('High: ' + json_data['Time Series (Daily)'][day]['2. high'])
     high_array.append(json_data['Time Series (Daily)'][day]['2. high'])
     # print('Low: ' + json_data['Time Series (Daily)'][day]['3. low'])
@@ -43,7 +44,7 @@ for day in json_data['Time Series (Daily)']:
     # print('Adjusted Close: ' + json_data['Time Series (Daily)'][day]['5. adjusted close'])
     ad_close_array.append(float(json_data['Time Series (Daily)'][day]['5. adjusted close']))
     # print('Volume: ' + json_data['Time Series (Daily)'][day]['6. volume'])
-    volume_array.append(json_data['Time Series (Daily)'][day]['6. volume'])
+    volume_array.append(float(json_data['Time Series (Daily)'][day]['6. volume']))
     # print('Dividend Amount: ' + json_data['Time Series (Daily)'][day]['7. dividend amount'])
     div_array.append(json_data['Time Series (Daily)'][day]['7. dividend amount'])
     # print('Split Coefficient: ' + json_data['Time Series (Daily)'][day]['8. split coefficient'])
@@ -64,3 +65,9 @@ stock_df = pd.DataFrame(data_dict)
 stock_df.plot(kind='line',x='date',y='ad_close')
 
 plt.show()
+
+model = LinearRegression().fit(stock_df['open'].values.reshape((-1, 1)), stock_df['ad_close'])
+print(f'Intercept: {model.intercept_}')
+print(f'Coefficient: {model.coef_}')
+correlation = stock_df['ad_close'].corr(stock_df['open'])
+print(f'Correlation: {correlation}')
